da69fa066c3a5d0d8dc26492dafd2d71b950fe22
Gabriel Ferraz <gabrielmaiaferraz@gmail.com>
2024-08-20 21:03:21 -0300

    Added script containing initial distraction behaviour

---
+++ Assets/_Script/Oponents/GoblinBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace _Script.Oponents
+{
+  public class GoblinBehaviour : MonoBehaviour
+  {
+    private bool distracted = false;
+    private float distractedDuration;
+
+    public void Distract(float duration)
+    {
+      distracted = true;
+      distractedDuration = duration;
+    }
+
+    private void Update()
+    {
+      if (distracted && distractedDuration > 0)
+      {
+        distractedDuration -= Time.deltaTime;
+      }
+      else
+      {
+        distracted = false;
+      }
+    }
+  }
+}
